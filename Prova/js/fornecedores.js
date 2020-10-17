fe764dc31f2b68b4dfcefa8bbfb67926097673cf
GuiSM-Dev <69987315+GuiSM-Dev@users.noreply.github.com>
2020-10-17 15:02:26 -0300

    Add files via upload

---
+++ Prova/js/fornecedores.js
@@ -0,0 +1,44 @@
+const formulario = document.querySelector('#formularioFornecedores');
+const inputNome = document.querySelector('#nome');
+const inputEmail = document.querySelector('#email');
+const inputProduto = document.querySelector('#produto');
+const inputTelefone = document.querySelector('#telefone')
+const fornecedores = document.querySelector('#fornecedores');
+const mensagens = document.querySelector('#mensagens');
+const emails = []
+
+formulario.addEventListener('submit', (e) => {
+    e.preventDefault();
+    
+    if (inputNome.value === '' || inputEmail.value === '' || inputProduto.value === '' || inputTelefone.value === '') {
+        mensagens.style.display = 'block';
+        mensagens.style.background = 'red';
+        mensagens.innerHTML = 'Preencha todos os campos!';
+    } 
+
+    else if (emails.includes(inputEmail.value)==true) {
+        alert("Email já cadastrado, tente outro!")
+    }
+
+    
+    else {
+        emails.push(inputEmail.value)
+        const li = document.createElement('li');
+        li.innerHTML = `Nome: ${inputNome.value} <br>Email: ${inputEmail.value} <br>Produto: ${inputProduto.value} <br>Telefone: ${inputTelefone.value}`;
+        li.addEventListener('click', (e) => {
+            const deletar = confirm('Você realmente gostaria de deletar este fornecedor?');
+
+            if (deletar) {
+                e.target.remove();
+            }
+        })
+
+        fornecedores.appendChild(li);
+        mensagens.style.display = 'none';
+
+        inputNome.value = '';
+        inputEmail.value = '';
+        inputProduto.value = '';
+        inputTelefone.value = '';
+    }
+})
